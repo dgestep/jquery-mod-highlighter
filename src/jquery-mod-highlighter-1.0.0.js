666561b5d271cc6816a65d98d94b5512f9204acf
dgestep <douglas.estep@gmail.com>
2012-12-15 15:25:35 -0500

    Initial Project Setup

---
+++ src/jquery-mod-highlighter-1.0.0.js
@@ -0,0 +1,15 @@
+/*!
+ * jQuery Modification Highlighter
+ * Author: Doug Estep.
+ * Version 1.0.0
+ * 
+ * API Documention:
+ *   http://dougestep.com/dme/jquery-mod-highlighter-widget 
+ * 
+ * Depends:
+ *   jquery 1.7.1 +
+ *   jquery-ui 1.8.20+ using the following plugins.
+ *     jQuery UI widget factory
+ */
+(function($, undefined) {
+})
